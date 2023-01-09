890610929850822897e1bcadf4902900a48cc02f
Luis Forbes <lu@Luiss-iMac.local>
2023-01-09 01:00:36 -0500

    ceated 'play_game' function, asking for user input on playing game vs player or game vs cpu

--- main.py/rules.py
+++ main.py/rules.py
@@ -7,3 +7,16 @@ class Rules:
         self.player1_counter = 0
         self.game_mode = input("would you like to play 'player vs player' or 'player vs cpu'?")
         self.player2_choice = input("Player 2, Would you like to choose rock, paper, scissors, lizard, or spock? ")
+
+
+        def play_game (self):
+        
+            self.choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+            self.valid_response = False
+            while self.valid_response == False:
+                self.game_mode = input("would you like to play 'player vs player' or 'player vs cpu'? ")
+                if self.game_mode == "player vs player":
+                    self.valid_response = True
+                elif self.game_mode == "player vs cpu":
+                    self.valid_response = True
+                print("")
