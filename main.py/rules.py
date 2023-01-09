c41f9acfc057d1562c48345ce3888ac9b8039c91
Luis Forbes <lu@Luiss-iMac.local>
2023-01-09 01:06:40 -0500

    imported 'time' into 'rules folder to call sleep function. created while loop to get user input for 'player vs player' game mode

--- main.py/rules.py
+++ main.py/rules.py
@@ -1,4 +1,4 @@
-
+import time
 
 class Rules:
     def __init__(self):
@@ -12,7 +12,10 @@ class Rules:
         def play_game (self):
         
             self.choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+            self.player1_counter = 0
+            self.player2_counter = 0
             self.valid_response = False
+            self.valid_response2 = False
             while self.valid_response == False:
                 self.game_mode = input("would you like to play 'player vs player' or 'player vs cpu'? ")
                 if self.game_mode == "player vs player":
@@ -20,3 +23,9 @@ class Rules:
                 elif self.game_mode == "player vs cpu":
                     self.valid_response = True
                 print("")
+            while self.player2_counter < 2 and self.player1_counter < 2:
+                if self.game_mode == "player vs player":
+                    print("")
+                    time.sleep(.8)
+                    self.player1_input = input("Player 1, Would you like to choose rock, paper, scissors, lizard, or spock? ")
+                    self.player2_choice = input("Player 2, Would you like to choose rock, paper, scissors, lizard, or spock? ")
