082b423eecaf187fdc7b39c6d36ea4a10de83b99
Luis Forbes <lu@Luiss-iMac.local>
2023-01-09 01:14:44 -0500

    created and defined rules for who would win if player 1 chose ____ and player 2/cpu chose ____

--- main.py/rules.py
+++ main.py/rules.py
@@ -35,3 +35,125 @@ class Rules:
                     time.sleep(.8)
                     self.player1_input = input("Player 1, Would you like to choose rock, paper, scissors, lizard, or spock? ")
                     self.player2_choice = random.choice(self.choices)
+                        
+                if self.player2_choice == "rock" and self.player1_input == "scissors":
+                    self.player2_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Rock crushes Scissors")
+
+                elif self.player2_choice == "scissors" and self.player1_input == "paper":
+                    self.player2_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Scissors cuts Paper")
+
+                elif self.player2_choice == "paper" and self.player1_input == "rock":
+                    self.player2_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Paper covers Rock")
+
+                elif self.player2_choice == "rock" and self.player1_input == "lizard":
+                    self.player2_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Rock crushes Lizard")
+
+                elif self.player2_choice == "lizard" and self.player1_input == "spock":
+                    self.player2_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Lizard poisons Spock")
+
+                elif self.player2_choice == "spock" and self.player1_input == "scissors":
+                    self.player2_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Spock smashes Scissors")
+
+                elif self.player2_choice == "scissors" and self.player1_input == "lizard":
+                    self.player2_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Scissors decapitates Lizard")
+
+                elif self.player2_choice == "lizard" and self.player1_input == "paper":
+                    self.player2_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Lizard eats Paper")
+
+                elif self.player2_choice == "paper" and self.player1_input == "spock":
+                    self.player2_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Paper disproves Spock")
+
+                elif self.player2_choice == "spock" and self.player1_input == "rock":
+                    self.player2_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Spock vaporizes Rock")
+
+
+
+                elif self.player1_input == "rock" and self.player2_choice == "scissors":
+                    self.player1_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Rock crushes Scissors")
+
+                elif self.player1_input == "scissors" and self.player2_choice == "paper":
+                    self.player1_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Scissors cuts Paper")
+
+                elif self.player1_input== "paper" and self.player2_choice == "rock":
+                    self.player1_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Paper covers Rock")
+
+                elif self.player1_input == "rock" and self.player2_choice == "lizard":
+                    self.player1_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Rock crushes Lizard")
+
+                elif self.player1_input == "lizard" and self.player2_choice == "spock":
+                    self.player1_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Lizard poisons Spock")
+
+                elif self.player1_input == "spock" and self.player2_choice == "scissors":
+                    self.player1_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Spock smashes Scissors")
+
+                elif self.player1_input == "scissors" and self.player2_choice == "lizard":
+                    self.player1_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Scissors decapitates Lizard")
+
+                elif self.player1_input == "lizard" and self.player2_choice == "paper":
+                    self.player1_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Lizard eats Paper")
+
+                elif self.player1_input == "paper" and self.player2_choice == "spock":
+                    self.player1_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Paper disproves Spock")
+
+                elif self.player1_input == "spock" and self.player2_choice == "rock":
+                    self.player1_counter += 1
+                    print(f"player2: ", self.player2_counter)
+                    print(f"player1: ", self.player1_counter)
+                    print("Spock vaporizes Rock")
